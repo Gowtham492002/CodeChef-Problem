e4381e70b37fc9000cbba6e676452a26cf673d29
Gowtham R <122969093+Gowtham492002@users.noreply.github.com>
2024-05-20 21:12:49 +0530

    Add files via upload

---
+++ AGENTCHEF.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+        a=int(input())
+        b=a/100
+        c=b*20
+        if 100%c==0:
+                print(int(100//c))
+        else:
+                print(int((100//c)+1))
